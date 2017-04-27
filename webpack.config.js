13900995f2de0ce8f0d40edef44357113b8f7754
Rhed Shi <shi.rhed@gmail.com>
2017-04-27 02:01:27 -0700

    Add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: 'index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 module.exports = {
   entry: './client/index.js',
   output: {
-    filename: './bundle.js'
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
   },
   devServer: {
     port: 3000,
