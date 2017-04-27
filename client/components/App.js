d73f19f089c9170dc4f04c72e458fb9c6c81bca2
Rhed Shi <shi.rhed@gmail.com>
2017-04-27 04:30:29 -0700

    Add React

---
+++ client/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div style={{
+        backgroundColor: '#E6E6FA',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%'
+      }}>
+
+      </div>
+    );
+  }
+}
