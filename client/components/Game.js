35b41530bf88deeac2f0ad810d3146564ea48c8e
Rhed Shi <shi.rhed@gmail.com>
2017-04-29 06:58:59 -0700

    Add socket.io

--- client/components/Game.js
+++ client/components/Game.js
@@ -4,7 +4,23 @@ import styles from './game.css';
 
 import Board from './board.js';
 
+const io = require('socket.io-client');
+const socket = io();
+
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.game = props.match.params.game_id;
+  }
+
+  componentDidMount() {
+    socket.emit('game', {game: this.game, enter: true});
+  }
+
+  componentWillUnmount() {
+    socket.emit('game', {game: this.game, enter: false});
+  }
+
   render() {
     return (
       <div className={styles.root}>
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const webpack = require('webpack');
+const socket = require('socket.io');
 const path = require('path');
 const config = require('../webpack.config.js');
 
@@ -15,4 +16,23 @@ app.get('*', function(req, res) {
   res.sendFile('index.html', {root: path.resolve(__dirname, '../')});
 });
 
-app.listen(3000);
+const server = app.listen(3000);
+const io = socket(server);
+
+io.on('connection', function(socket) {
+  console.log('user connected');
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+
+  socket.on('game', function(data) {
+    if (data.enter) {
+      socket.join(data.game);
+      console.log('user joined game: ' + data.game);
+    } else {
+      socket.leave(data.game);
+      console.log('user left game: ' + data.game);
+    }
+  });
+});
