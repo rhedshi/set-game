2f2781f3e7061795b9f030feec5295be09279fc5
Rhed Shi <shi.rhed@gmail.com>
2017-04-29 03:04:34 -0700

    Remove html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
-
 module.exports = {
   entry: './client/index.js',
   output: {
