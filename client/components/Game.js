63fd0df6d43962258779b3a0acd3fd5267a955d9
Rhed Shi <rhed@fb.com>
2018-11-08 22:26:38 -0800

    Add Set Game title to home page with redesign

--- client/components/Game.js
+++ client/components/Game.js
@@ -8,10 +8,21 @@ const io = require('socket.io-client');
 const socket = io();
 
 export default class Game extends React.Component {
+  renderTitle() {
+    return (
+      <div className={styles.title}>
+        <img src={require('../assets/set-game-title.svg')}/>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className={styles.root}>
+        {this.renderTitle()}
+        <span></span>
         <Board/>
+        <span></span>
       </div>
     );
   }
