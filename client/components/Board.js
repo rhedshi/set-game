2babd34ccc2664907c5a9da9b8d38ba2317c0a10
Rhed Shi <rhedshi@users.noreply.github.com>
2026-04-03 07:00:08 +0100

    Replace cards using scheduled timeout instead of CSSTransition

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  CSSTransition,
-  TransitionGroup,
-} from 'react-transition-group'
 import styles from '../styles/board.css'
 
 import Card from './Card.js'
@@ -13,15 +9,8 @@ import _ from 'lodash'
 /** Must match `.selected.failure` animation duration in `card.css` */
 const FAILURE_ANIMATION_MS = 750
 
-const CardTransition = (props) => {
-  return (
-    <CSSTransition
-      {...props}
-      classNames={{ enter: styles.enter }}
-      timeout={750}
-    />
-  )
-}
+/** Must match `.selected.success` animation duration in `card.css` */
+const SUCCESS_ANIMATION_MS = 750
 
 export default class Board extends React.Component {
   constructor(props) {
@@ -31,10 +20,13 @@ export default class Board extends React.Component {
     }
     this.failureDeselectTimeout = null
     this.failureDeselectKey = null
+    this.successReplaceTimeout = null
+    this.successReplaceKey = null
   }
 
   componentWillUnmount() {
     this.clearFailureDeselectTimer()
+    this.clearSuccessReplaceTimer()
   }
 
   clearFailureDeselectTimer() {
@@ -43,6 +35,14 @@ export default class Board extends React.Component {
       this.failureDeselectTimeout = null
     }
     this.failureDeselectKey = null
+  }
+
+  clearSuccessReplaceTimer() {
+    if (this.successReplaceTimeout) {
+      clearTimeout(this.successReplaceTimeout)
+      this.successReplaceTimeout = null
+    }
+    this.successReplaceKey = null
   }
 
   scheduleFailureDeselect(ids) {
@@ -59,24 +59,38 @@ export default class Board extends React.Component {
     }, FAILURE_ANIMATION_MS)
   }
 
+  scheduleSuccessReplace(ids) {
+    const key = ids.slice().sort((a, b) => a - b).join('-')
+    if (this.successReplaceKey === key && this.successReplaceTimeout) {
+      return
+    }
+    this.clearSuccessReplaceTimer()
+    this.successReplaceKey = key
+    if (!this.state.found) {
+      this.setState({ found: true })
+    }
+    this.successReplaceTimeout = setTimeout(() => {
+      this.successReplaceTimeout = null
+      this.successReplaceKey = null
+      this.props.replaceCards(ids)
+    }, SUCCESS_ANIMATION_MS)
+  }
+
   componentDidUpdate(prevProps, prevState, snapshot) {
     const cards = this.selectedCards()
     if (cards.length === 3) {
       if (isValidSet(..._.map(cards, 'id'))) {
         this.clearFailureDeselectTimer()
-        if (!this.state.found) {
-          this.setState({ found: true })
-        }
-        else {
-          this.props.replaceCards(_.map(cards, 'id'))
-        }
+        this.scheduleSuccessReplace(_.map(cards, 'id'))
       }
       else {
+        this.clearSuccessReplaceTimer()
         this.scheduleFailureDeselect(_.map(cards, 'id'))
       }
     }
     else {
       this.clearFailureDeselectTimer()
+      this.clearSuccessReplaceTimer()
       if (this.state.found) {
         this.setState({ found: false })
       }
@@ -94,39 +108,30 @@ export default class Board extends React.Component {
   }
 
   renderCards() {
-    const cards = this.props.cards.map((card, index) => {
+    return this.props.cards.map((card, index) => {
       return (
-        <CardTransition key={card.id}>
-          <div>
-            <Card
-              key={card.id}
-              color={card.color}
-              fill={card.fill}
-              number={card.number}
-              shape={card.shape}
-              selected={card.selected}
-              success={card.selected && this.state.found}
-              failure={
-                card.selected &&
-                this.selectedCards().length === 3 &&
-                !this.state.found
-              }
-              onClick={() => this.onCardClicked(card, index)}
-            />
-          </div>
-        </CardTransition>
+        <div key={card.id}>
+          <Card
+            color={card.color}
+            fill={card.fill}
+            number={card.number}
+            shape={card.shape}
+            selected={card.selected}
+            success={card.selected && this.state.found}
+            failure={
+              card.selected &&
+              this.selectedCards().length === 3 &&
+              !this.state.found
+            }
+            onClick={() => this.onCardClicked(card, index)}
+          />
+        </div>
       )
     })
-
-    return cards
   }
 
   render() {
-    return (
-      <TransitionGroup className={styles.root}>
-        {this.renderCards()}
-      </TransitionGroup>
-    )
+    return <div className={styles.root}>{this.renderCards()}</div>
   }
 }
 
